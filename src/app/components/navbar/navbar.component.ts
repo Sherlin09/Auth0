9b45d58c7511dca48fc00b183c2c3f3f97352e28
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2020-10-27 11:26:57 -0600

    6 boton logout

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,5 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
@@ -8,7 +10,7 @@ import { AuthService } from '@auth0/auth0-angular';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor( public auth: AuthService ) { }
+  constructor(@Inject(DOCUMENT) public document: Document,  public auth: AuthService ) { }
 
   ngOnInit(): void {
   }
